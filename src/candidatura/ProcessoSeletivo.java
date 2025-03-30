cbc3f46aded70f892c2f9845df0f67c6d9840f8f
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-03-30 09:44:06 -0300

    Adicionado o método analisarCandidatos para validação dos candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -7,4 +7,19 @@ public class ProcessoSeletivo {
 
 	}
 
+	public static void analisarCandidatura(double salarioPretendido) {
+		double salarioBase = 2000.0;
+
+		// Verificar se o salário pretendido é menor, igual ou maior que o salário base.
+		if (salarioPretendido < salarioBase) {
+			System.out.println("Ligar para o candidato!");
+			
+		} else if (salarioPretendido == salarioBase) {
+			System.out.println("Ligar para o candidato com contraproposta!");
+			
+		} else {
+			System.out.println("Aguardando o resultado dos demais candidatos!");
+		}
+	}
+
 }
